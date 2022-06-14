7b7e62fb678bdb0709808d0e0f00c639304f4067
ItzLightyHD <itzlightyhd@apexie.eu>
2022-06-14 09:24:51 +0200

    Just trying to help ;)

--- src/ItzLightyHD/KnockbackFFA/utils/KnockbackPlayer.php
+++ src/ItzLightyHD/KnockbackFFA/utils/KnockbackPlayer.php
@@ -9,8 +9,11 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerJumpEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+use pocketmine\network\mcpe\protocol\types\PlayerAuthInputFlags;
 use pocketmine\player\Player;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\Server;
@@ -24,6 +27,8 @@ class KnockbackPlayer implements Listener
     public array $jumptask = [];
     public array $lastDmg = [];
     public array $killstreak = [];
+    public array $jump_queue = [];
+    public array $double_jump_queue = [];
     /** @var Loader $plugin */
     private Loader $plugin;
 
@@ -40,7 +45,7 @@ class KnockbackPlayer implements Listener
         $this->lastDmg[$name] = "none";
         $this->jumpcount[$name] = 0;
         $this->killstreak[$name] = 0;
-        if(!isset(EssentialsListener::$cooldown[$player->getName()])) {
+        if (!isset(EssentialsListener::$cooldown[$player->getName()])) {
             EssentialsListener::$cooldown[$player->getName()] = 0;
         }
         if ($player->getWorld()->getFolderName() === GameSettings::getInstance()->world) {
@@ -71,36 +76,50 @@ class KnockbackPlayer implements Listener
     {
         if (GameSettings::getInstance()->doublejump) return;
         $player = $event->getPlayer();
+        if ($player->getWorld()->getFolderName() === GameSettings::getInstance()->world) $this->jumpQueue[$player->getName()] = microtime(true);
+    }
+
+    public function onDataPacketReceive(DataPacketReceiveEvent $event): void
+    {
+        if (GameSettings::getInstance()->doublejump) return;
+        $player = $event->getOrigin()->getPlayer();
+        $packet = $event->getPacket();
+        if (!$packet instanceof PlayerAuthInputPacket) {
+            return;
+        }
+        if ($player === null) {
+            return;
+        }
+        if (!$packet->hasFlag(PlayerAuthInputFlags::JUMP_DOWN)) {
+            return;
+        }
+        if (!isset($this->jump_queue[$player->getName()])) {
+            return;
+        }
+        if (microtime(true) - $this->jump_queue[$player->getName()] < 0.1) {
+            return;
+        }
         if (!isset(EssentialsListener::$cooldown[$player->getName()])) {
             EssentialsListener::$cooldown[$player->getName()] = 0;
         }
-        if (!isset($this->jumpcount[strtolower($player->getName())])) {
+        if (isset($this->double_jump_queue[$player->getName()]) && microtime(true) - $this->jump_queue[$player->getName()] < 0.3) {
+            return;
+        }
+        $this->double_jump_queue[$player->getName()] = microtime(true);
+        if (EssentialsListener::$cooldown[$player->getName()] <= time()) {
+            $directionvector = $player->getDirectionVector()->multiply(4 / 2);
+            $dx = $directionvector->getX();
+            $dz = $directionvector->getZ();
+            $player->setMotion(new Vector3($dx, 1, $dz));
+            EssentialsListener::$cooldown[$player->getName()] = time() + 10;
             $this->jumpcount[strtolower($player->getName())] = 0;
+            unset($this->jumptask[strtolower($player->getName())]);
+        } else {
+            $player->sendMessage(GameSettings::getInstance()->getConfig()->get("prefix") . "§r§cWait §e" . (10 - ((time() + 10) - EssentialsListener::$cooldown[$player->getName()])) . "§c seconds before using your leap/double jump again.");
         }
-        if (($player->getWorld()->getFolderName() === GameSettings::getInstance()->world) && isset($this->jumpcount[strtolower($player->getName())])) {
-            $this->jumpcount[strtolower($player->getName())]++;
-            if (!isset($this->jumptask[strtolower($player->getName())])) {
-                $this->jumptask[strtolower($player->getName())] = Loader::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($player): void {
-                    $this->jumpcount[strtolower($player->getName())] = 0;
-                    unset($this->jumptask[strtolower($player->getName())]);
-                }), 30);
-            }
-            if ($this->jumpcount[strtolower($player->getName())] === 2) {
-                if (EssentialsListener::$cooldown[$player->getName()] <= time()) {
-                    $directionvector = $player->getDirectionVector()->multiply(4 / 2);
-                    $dx = $directionvector->getX();
-                    $dz = $directionvector->getZ();
-                    $player->setMotion(new Vector3($dx, 1, $dz));
-                    EssentialsListener::$cooldown[$player->getName()] = time() + 10;
-                    $this->jumpcount[strtolower($player->getName())] = 0;
-                    unset($this->jumptask[strtolower($player->getName())]);
-                } else {
-                    $player->sendMessage(GameSettings::getInstance()->getConfig()->get("prefix") . "§r§cWait §e" . (10 - ((time() + 10) - EssentialsListener::$cooldown[$player->getName()])) . "§c seconds before using your leap/double jump again.");
-                }
-            }
-        }
+        unset($this->jump_queue[$player->getName()]);
     }
-  
+
     public function playSound(string $soundName, ?Player $player): void
     {
         $pk = new PlaySoundPacket();
